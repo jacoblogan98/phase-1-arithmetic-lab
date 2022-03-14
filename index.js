38555530583e4e766a6a512d4f7985aa4abf888e
Jacob Logan <jacoblogan98@gmail.com>
2022-03-14 13:59:10 -0400

    commit tree 041273a8a09851111653642d5990c4505d49b845

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 62 
+const num2 = 1
+let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random()*10);
+
+const num3 = 16
+const num4 = 6
+let mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,5,20);
